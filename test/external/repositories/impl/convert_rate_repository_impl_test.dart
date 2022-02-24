ce06f8a2300915c239f7e29fde9b2907759148cc
Wilgner <wilgnerdelfino@gmail.com>
2022-02-24 15:22:07 -0300

    Asserted that the ConvertRateRepository call hive box correctly

--- test/external/repositories/impl/convert_rate_repository_impl_test.dart
+++ test/external/repositories/impl/convert_rate_repository_impl_test.dart
@@ -1,10 +1,21 @@
 import 'package:app_tdd/business/repositories/convert_rate_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import './convert_rate_repository_impl_test.mocks.dart';
+
+@GenerateMocks([HiveInterface])
 void main() {
-  final repository = ConvertRateRepositoryImpl();
-  test("should call the hive box correctly", () {
+  final mockHive = MockHiveInterface();
+  final repository = ConvertRateRepositoryImpl(mockHive);
+  test("should call the hive box correctly", () async {
     //given
+    await Hive.initFlutter();
+    final box = await Hive.openBox('test');
+    when(mockHive.box('convertRates')).thenReturn(box);
 
     //when
     repository.getConvertRate(
@@ -13,16 +24,21 @@ void main() {
     );
 
     //then
+    verify(mockHive.box('convertRates'));
   });
 }
 
 class ConvertRateRepositoryImpl extends ConvertRateRepository {
+  final HiveInterface hive;
+
+  ConvertRateRepositoryImpl(this.hive);
+
   @override
   double getConvertRate({
     required String sourceId,
     required String destinationId,
   }) {
-    // TODO: implement getConvertRate
-    throw UnimplementedError();
+    hive.box('convertRates');
+    return 0.0;
   }
 }
